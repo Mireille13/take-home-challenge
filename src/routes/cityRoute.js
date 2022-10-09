cb49d03de5e96d85627910323a370897b3658be8
mireille.crocco@gmail.com <mireille.crocco@gmail.com>
2022-10-09 15:24:09 -0400

    tentative routes

---
+++ src/routes/cityRoute.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const cityRoute = express.Router();
+
+// const eventController = require('../controller/eventController.js');
+
+// cityRoute.get('/', eventController.getCity, (req, res) =>{
+//     return res.status(200).json(res.locals.cityList);
+// });
+
+//module.exports = cityRoute;
---
+++ src/routes/eventRoute.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const eventRoute = express.Router();
+
+// const eventController = require('../controller/eventController.js');
+
+// eventRoute.get('/', eventController.getEvents, (req, res) => {
+//     return res.status(200).json(res.locals.eventList);
+// });
+
+// module.exports = eventRoute;
